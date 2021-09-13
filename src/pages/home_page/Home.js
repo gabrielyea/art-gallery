2055af6a37ce58d2a6d209ef3f014b7bea807a20
Gabriel <gabriel.gabrielyea@gmail.com>
2021-09-13 16:35:46 -0500

    Add style and animations to home

--- src/pages/home_page/Home.js
+++ src/pages/home_page/Home.js
@@ -1,16 +1,30 @@
-/* eslint-disable arrow-body-style */
+/* eslint-disable no-unused-vars */
+import useWindowDimensions from '../../components/customHooks/useWindowDimensions';
+import DecorationBar from '../../components/decoration_bar/DecorationBar';
 import Search from '../../components/search/Search';
 import styles from './homeStyle.module.scss';
 
 const Home = () => {
+  const [x, y] = useWindowDimensions();
+
   return (
     <div className={styles.mainContainer}>
+      <DecorationBar
+        totalSize={x}
+        internalPos={{ x, y: '40' }}
+        screenPos={{ top: 250, right: 0 }}
+      />
       <div className={styles.titleContainer}>
         <h1>
           Welcome to the gallery.
         </h1>
       </div>
       <Search />
+      <DecorationBar
+        totalSize={x}
+        internalPos={{ x: '40', y: '40' }}
+        screenPos={{ top: 700, right: 0 }}
+      />
     </div>
   );
 };
