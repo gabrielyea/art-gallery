d2e18983ba8dd8479dd768ba854519cc8a0ffb6f
Gabriel <gabriel.gabrielyea@gmail.com>
2021-09-13 16:35:29 -0500

    Add style and animations to Search

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,24 +1,56 @@
+import { motion } from 'framer-motion';
 import { RiSearchLine } from 'react-icons/ri';
 import styles from './searchStyle.module.scss';
 
+const container = {
+  initial: {
+  },
+  animate: {
+    transition: {
+      staggerChildren: 0.2,
+      delayChildren: 0.2,
+    },
+  },
+};
+
+const children1 = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+  },
+};
+
+const children2 = {
+  initial: {
+    scale: 0,
+    opacity: 0,
+  },
+  animate: {
+    scale: 1,
+    opacity: 1,
+  },
+};
+
 const Search = () => (
 
-  <div className={styles.mainContainer}>
-    <form>
+  <motion.div className={styles.mainContainer}>
+    <motion.form variants={container} initial="initial" animate="animate">
       <div className={styles.optionsContainer}>
-        <div className={styles.option}> Author </div>
-        <div className={styles.selected}> Work name </div>
+        <motion.div variants={children2} className={styles.option}> Author </motion.div>
+        <motion.div variants={children2} className={styles.selected}> Work name </motion.div>
       </div>
       <div className={styles.inputContainer}>
-        <div>
+        <motion.div variants={children1}>
           <input type="text" />
-        </div>
-        <button type="submit">
+        </motion.div>
+        <motion.button variants={children1} type="submit">
           <RiSearchLine className={styles.icon} />
-        </button>
+        </motion.button>
       </div>
-    </form>
-  </div>
+    </motion.form>
+  </motion.div>
 );
 
 export default Search;
