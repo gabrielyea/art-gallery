957b2884f2daddc54a25256a0a62ca353fdd7aa0
Gabriel <gabriel.gabrielyea@gmail.com>
2021-09-25 16:00:53 -0500

    Set max queries

--- src/components/customHooks/useDataGetter.js
+++ src/components/customHooks/useDataGetter.js
@@ -3,17 +3,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../search/searchSlice';
 
 const useDataGetter = () => {
-  const list = useSelector((state) => state.list.entities);
+  const { entities, error } = useSelector((state) => state.list);
   const dispatch = useDispatch();
+  let max = 1;
 
   const addOffset = (arr, offset) => arr.map((v) => v + offset);
 
+  const setMaxQueries = () => {
+    if (entities.length < 4 && error === null) {
+      max = entities.length;
+    } else {
+      max = 4;
+    }
+  };
+
   const getFromList = useCallback((offset) => {
-    let arr = [...Array(4).keys()];
-    arr = addOffset(arr, offset);
-    const que = arr.map((q) => `https://collectionapi.metmuseum.org/public/collection/v1/objects/${list[q]}`);
-    dispatch(fetchData([...que]));
-  }, [list]);
+    setMaxQueries();
+    if (error === null) {
+      let arr = [...Array(max).keys()];
+      arr = addOffset(arr, offset);
+      const que = arr.map((q) => `https://collectionapi.metmuseum.org/public/collection/v1/objects/${entities[q]}`);
+      dispatch(fetchData([...que]));
+    }
+  }, [entities]);
 
   return { getFromList };
 };
